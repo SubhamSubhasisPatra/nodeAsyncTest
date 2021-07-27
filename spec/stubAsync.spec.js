fae8df271021f8e5c9ec7eb4a7065764383e40a9
subhasis <subhasis@subhasiss-MacBook-Air.local>
2021-07-27 09:08:21 +0530

    added how to work with spy in stubAsync.js file

--- spec/stubAsync.spec.js
+++ spec/stubAsync.spec.js
@@ -1,6 +1,7 @@
 const expect = require("chai").expect;
 const Test = require("../src/stubAsyn");
 const sinon = require("sinon");
+const assert = require("assert");
 
 describe("Check for the async function", () => {
   let test;
@@ -9,11 +10,14 @@ describe("Check for the async function", () => {
     expect(test).to.be.an.instanceof(Test);
   });
 
-  it("should check with a stub", async function () {
-    const stub = sinon.stub(test, "addPromise");
-    expect(await test.addPromise.firstCall.args.length).to.equal(2);
-    // expect(await test.addPromise(1, 2)).to.be.equal(3);
-    // let res = await test.addPromise(10, 20);
-    // console.log(res);
+  it("should check with a Spy", async function () {
+    const spy = sinon.spy(test, "addPromise");
+    let res = await test.addPromise(1, 2);
+    expect(res).to.be.equal(3);
+    expect(spy.calledOnce).to.be.true;
+    expect(spy.calledWith(1, 2)).to.be.true;
+    expect(spy.calledOn(test)).to.be.true;
+    expect(test.addPromise.firstCall.args.length).to.equal(2);
+    spy.restore();
   });
 });
