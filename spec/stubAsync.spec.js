30c4814917115309c04740fd994b778f1750c0ac
subhasis <subhasis@subhasiss-MacBook-Air.local>
2021-07-27 22:11:23 +0530

    Added the UT for external API call

--- spec/stubAsync.spec.js
+++ spec/stubAsync.spec.js
@@ -11,13 +11,24 @@ describe("Check for the async function", () => {
   });
 
   it("should check with a Spy", async function () {
+    this.timeout(0);
     const spy = sinon.spy(test, "addPromise");
     let res = await test.addPromise(1, 2);
     expect(res).to.be.equal(3);
     expect(spy.calledOnce).to.be.true;
     expect(spy.calledWith(1, 2)).to.be.true;
     expect(spy.calledOn(test)).to.be.true;
-    expect(test.addPromise.firstCall.args.length).to.equal(2);
+    expect(test.addPromise.getCall(0).args.length).to.equal(2);
     spy.restore();
+  });
+
+  it("Shoud check for the getter method", async function () {
+    this.timeout(0);
+    const spy = sinon.spy(test, "dummyData");
+    let res = await test.dummyData();
+    expect(typeof res).to.be.deep.equal("object");
+    expect(res.status).to.be.equal(200);
+    expect(spy.calledOnce).to.be.true;
+    expect(test.dummyData.getCall(0).args.length).to.equal(0);
   });
 });
--- src/stubAsyn.js
+++ src/stubAsyn.js
@@ -1,5 +1,17 @@
+const fetch = require("node-fetch");
+
 module.exports = class Test {
   addPromise = (a, b) => {
-    return Promise.resolve(a + b);
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(a + b);
+      }, 1000);
+    });
   };
+
+  async dummyData() {
+    const url = "https://jsonplaceholder.typicode.com/todos/1";
+    let res = await fetch(url);
+    return res;
+  }
 };
