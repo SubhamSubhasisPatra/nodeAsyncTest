169ca6d3aced856dadd1ac6e7fcdafa97d36e2af
subhasis <subhasis@subhasiss-MacBook-Air.local>
2021-08-03 00:56:03 +0530

    added the reject devInfo UT

--- spec/innerAsync.spec.js
+++ spec/innerAsync.spec.js
@@ -3,7 +3,7 @@ const expect = require("chai").expect;
 const sinon = require("sinon");
 
 describe("Test for the inner Promise ", () => {
-  it("check for the instance", async function () {
+  it("check for the Resolved Dev info", async function () {
     this.timeout(0);
     let dummyData = {
       userType: "normal",
@@ -15,15 +15,34 @@ describe("Test for the inner Promise ", () => {
       user: "superUser",
       password: "superPassword",
     };
-    const device = new Device({ id: 1, name: "subham" });
-    expect(device).to.be.an.instanceof(Device);
+    try {
+      const device = new Device({ id: 1, name: "subham" });
+      expect(device).to.be.an.instanceof(Device);
 
-    let getDeviceInfoStub = sinon
-      .stub(Device, "getDeviceInfo")
-      .resolves(dummyData);
+      let getDeviceInfoStub = sinon
+        .stub(Device, "getDeviceInfo")
+        .resolves(dummyData);
 
-    let result = await device.info({ id: 1 });
-    expect(result).to.deep.equal(expectedResult);
-    getDeviceInfoStub.restore();
+      let result = await device.info({ id: 1 });
+      expect(result).to.deep.equal(expectedResult);
+      getDeviceInfoStub.restore();
+    } catch (err) {
+      expect(err.message).to.be.equal("Error in info");
+    }
+  });
+
+  it("shoud cehck for the rejected devInfo", async function () {
+    this.timeout(0);
+    try {
+      const device = new Device({ id: 1, name: "subham" });
+      let expectedResult = {
+        userType: "normal",
+        time: "12:00",
+        user: "superUser",
+        password: "superPassword",
+      };
+      let result = await device.info({ idid: 1 });
+      expect(result).to.deep.equal(expectedResult);
+    } catch (err) {}
   });
 });
